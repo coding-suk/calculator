e11c3fc59eec014bd04560e9d088b0f9cbcd4d95
developer1 <dev1@email.com>
2024-02-14 02:57:54 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ class Calculator(QWidget):
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
 
-        # self.btn2=QPushButton('Clear',self)
-        # self.btn2.clicked.connect(self.clearMessage)
+        self.btn2=QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
 
-        # hbox = QHBoxLayout()
-        # hbox.addStretch(1)
-        # hbox.addWidget(self.btn1)
-        # hbox.addWidget(self.btn2)
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
-        # vbox.addLayout(hbox)
+        # vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -41,8 +41,8 @@ class Calculator(QWidget):
         self.resize(256,256)
         self.show()
 
-    # def clearMessage(self):
-    #     self.te1.clear()
+    def clearMessage(self):
+        self.te1.clear()
 
     def activateMessage(self):
         # QMessageBox.information(self,"information","Button clicked!")
